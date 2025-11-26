1fb82c637dddc958cbf62cd3f714361c9275ff96
boatingstephen58-sketch <boatingstephen58@gmail.com>
2025-11-26 07:15:15 +0300

    Add files via upload

---
+++ boat.py
@@ -0,0 +1,15 @@
+import typing
+text = "hello world"
+messagge = "welcome to Asonomaso"
+print(text and messagge )
+full = f"{text} \n {messagge}"
+print (full) 
+print(text.find("pro"))
+print(messagge.rsplit())
+print(False)
+if text == messagge: {}
+if messagge > text :
+   print(True)
+print("act" not in text )
+print("world" and "Asonomaso" in text and messagge)
+
